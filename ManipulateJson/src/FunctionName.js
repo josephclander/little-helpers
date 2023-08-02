d9ecf16a1207881da51532d236051148040a53d3
Joseph Lander <josephclander@gmail.com>
2023-08-02 16:39:20 +0100

    Initial commit

---
+++ ManipulateJson/src/FunctionName.js
@@ -0,0 +1,24 @@
+/**
+ * Convert JSON into the Cyclr output
+ * @param {object} input
+ * @return {json}
+ */
+function functionName(input) {
+  /*
+    Required output for Cyclr fields
+      {
+        "Values": [],
+        "ConnectorField": "string",
+        "SystemField": "String",
+        "DataType": "Text",
+        "Description": "String",
+        "DisplayName": "String",
+        "DisplayOrder": 1
+      }
+  */
+ 
+
+  return JSON.stringify(output);
+}
+
+module.exports = functionName;
