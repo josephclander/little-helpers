8ab3371d4f7c6b4d3eac89601153b1b7c08fe222
Joseph Lander <josephclander@gmail.com>
2023-09-19 17:01:37 +0100

    Add first working level

--- ScrapeInfoFromHTML/index.js
+++ ScrapeInfoFromHTML/index.js
@@ -6,34 +6,30 @@ JSDOM.fromFile("inputData.html").then((dom) => {
   // elements that start with property-name
   const list = dom.window.document.querySelectorAll(
     "[data-test^='property-name']"
-    // "[data-test='property-name-product_handle']"
   );
-  // console.log(list[0].textContent); // product_handle
-  // console.log(list[0].textContent); // product_handle
+
   for (let i = 0; i < list.length; i++) {
     const object = {};
-    console.log(list[i].parentElement.parentElement.nextSibling.textContent);
+
     object["Values"] = [];
     object["ConnectorField"] = list[i].textContent.trim();
     object["DataType"] =
       list[i].parentElement.lastElementChild.textContent.trim();
-    object["Description"] = list[i].parentElement.parentElement.nextSibling
-      .firstChild
-      ? list[i].parentElement.parentElement.nextSibling.firstChild.textContent
+    object["Description"] = list[i].parentElement.parentElement
+      .nextElementSibling
+      ? list[i].parentElement.parentElement.nextElementSibling.textContent
+          .replace(/\s{2,}/g, "")
+          .trim()
       : "";
     object["DisplayOrder"] = i + 1;
     fields.push(object);
   }
 
-  // object["ConnectorField"] = el.innerText;
-  // object["Description"] =
-  //   el.parentElement.parentElement.nextSibling.firstChild.textContent;
   console.log(JSON.stringify(fields, " ", 4));
 });
 
-// finding the name
-// const list = dom.window.document.querySelector(
-//   // "[data-test^='property-name']"
-//   "[data-test='property-name-product_handle']"
-// ).textContent.trim();
-// console.log(JSON.stringify(list));
+
+
+// useful traversal commmands
+
+// nextSibling
